7d1dea8d13c9831db812dd08dff56b4099bdc3b3
arenuia <arenui.anderson@gmail.com>
2018-10-12 16:20:28 -0600

    Update linearRegression.py

--- linearRegression.py
+++ linearRegression.py
@@ -20,6 +20,7 @@ cwd = os.getcwd()
 # 3. The errors are independent of each other.
 
 ########## Part1 ##########
+
 print("########## Part 1 ##########")
 hwdata = pd.read_csv(os.path.join(cwd, 'height_weight1.csv'))
 hwdata.columns = ['height', 'weight']
@@ -66,7 +67,6 @@ plt.plot(no_int_model.predict(hwdataTrain), no_int_model.resid, '.')
 
 # This is the last thing that I really looked at. The rest is mainly changing names to be descriptive and taking away redundant stuff.
 
-#(1)TODO:have a stat guy look at
 # The R-squared variance is less for our linear model with the intercept meaning that our data fits the first linear model better. The error variance is within a near range of y for each x as x and y increases, this is more true for our first model than our second so we can say this is a better fit. The errors are independent of each other we see this by the absence of any major defects in our graph along with no abnormal clumps of data in either graph.
 #If we were to use R^2 as our function then our model then our residual would be at least even greater, so it wont fit the data better.
 
